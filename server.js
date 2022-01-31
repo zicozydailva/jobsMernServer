eea3110381edaa267652d8c29db8863c4d1348b9
Ogbu Ezekiel Ike <zico@Macbooks-MacBook-Pro.local>
2022-01-31 23:58:04 +0100

    server setup, error middleware implementation

---
+++ server.js
@@ -0,0 +1,24 @@
+// setting "type": "module" in package.json allowed us to be able to use ES6 imports instead of regular JS.. i.e from const -> import
+
+
+import express from "express";
+import dotenv from "dotenv"
+dotenv.config()
+const app = express();
+const port = process.env.PORT || 500
+
+// middleware
+import notFoundMiddleware from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+
+app.get("/", (req, res) => {
+    throw new Error("error")
+    res.send("Working")
+})
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
