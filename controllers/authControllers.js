829f3027fdeadab6298e602731d2ecc1dc175683
Ogbu Ezekiel Ike <zico@Macbooks-MacBook-Pro.local>
2022-02-02 21:48:35 +0100

    add job functionality ...backend

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -49,7 +49,21 @@ const login = async (req, res) => {
 };
 
 const updateUser = async (req, res) => {
-  res.send("updateUser");
+  const {email, name, lastName, location} = req.body
+  if(!email || !name || !lastName || !location) {
+    throw new BadRequestError("Invalid Credentials")
+  }
+  const user =await User.findOne({_id: req.user.userId})
+
+  user.email = email
+  user.name = name
+  user.lastName = lastName
+  user.location = location
+
+  await user.save()
+  const token = user.createJWT()
+  res.status(StatusCodes.OK).json({user, token, location:user.location})
 };
+
 
 export { register, login, updateUser };
--- controllers/jobControllers.js
+++ controllers/jobControllers.js
@@ -1,6 +1,16 @@
+import { StatusCodes } from "http-status-codes"
+import { BadRequestError } from "../errors/index.js"
+import Job from "../models/Job.js"
 
 const createJob = async (req, res) => {
-    res.send("create job")
+    const {position, company} = req.body
+
+    if(!position || !company) {
+       throw new BadRequestError("Please provide all values")
+    }
+    req.body.createdBy = req.user.userId;
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json(job)
 }
 
 const deleteJob = async (req, res) => {
